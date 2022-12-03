26bb4d1d67479ff816ec1378fa1f435672780924
hayun <hs183000@naver.com>
2022-12-03 22:45:19 +0900

    오류로 인한 지웠다 새로깔기

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "886dba6c33e264d34c0abd935132f119",
-    language: "ko_KR",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
+    language: "ko-KR",
   },
 });
 
