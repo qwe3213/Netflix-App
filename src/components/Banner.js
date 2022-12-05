b8e329876149bc3bbe49fc62f69d409f683066cf
hayun <hs183000@naver.com>
2022-12-05 17:02:30 +0900

    banner 다양한 기능 설정

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
 import requests from "../api/requests";
+import "./Banner.css";
 export default function Banner() {
   const [movie, setMovie] = useState([]);
 
@@ -22,6 +23,29 @@ export default function Banner() {
     setMovie(movieDetail);
     //data를 movieDetail로 설정후 setMovie에 movieDetail을 넣기
     //특정 영화의 더 상세정보를 가져오기(비디오 정보 포함)
+
+    // const truncate = (str, n) => {
+    //   return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    // };
   };
-  return;
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1> {movie.title || movie.name || movie.original_name}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button play">Play </button>
+          <button className="banner__button info">More Information</button>
+        </div>
+        <h1 className="banner__description">{movie.overview}</h1>
+      </div>
+      <div className="banner__fadeBottom" />
+    </header>
+  );
 }
