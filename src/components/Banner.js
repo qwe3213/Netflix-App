471fe62fd829f64dcce42b84ea286634c43338ff
hayun <hs183000@naver.com>
2022-12-05 16:35:55 +0900

    특정 영화의 상세정보 더가져오기

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,17 +8,20 @@ export default function Banner() {
     fetchData();
   }, []);
   const fetchData = async () => {
-    // 현재 상영중인 영화  정보를 가져오기 (여러영화)
     const request = await axios.get(requests.fetchNowPlaying);
-    //여러 영화 중 영화 하나의 ID 가져오기
+    // 현재 상영중인 영화  정보를 가져오기 (여러영화)
+
     const movieId =
-      request.data.result[
-        Math.floor(Math.random() * request.data.result.length)
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
       ].id;
+    //여러 영화 중 영화 하나의 ID 가져오기
     const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
       params: { append_to_reponse: "videos" },
     });
     setMovie(movieDetail);
+    //data를 movieDetail로 설정후 setMovie에 movieDetail을 넣기
+    //특정 영화의 더 상세정보를 가져오기(비디오 정보 포함)
   };
-  return <div></div>;
+  return;
 }
