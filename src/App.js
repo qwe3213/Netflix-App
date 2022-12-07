9e165b6e9fa1edf44f57abeaae82d191c28bacd9
hayun <hs183000@naver.com>
2022-12-07 15:54:25 +0900

    Row 컴포넌트 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Nav from "./components/Nav";
 import "./App.css";
+import Banner from "./components/Banner";
+import requests from "./api/requests";
+import Row from "./components/Row";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Banner />
+      <Row
+        title="NetflixOriginal"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow //화면 크기 크게하기
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </div>
   );
 }
