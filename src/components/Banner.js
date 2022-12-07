b17c3d789b9dcdc42d16cd6a178fda228ab0e865
hayun <hs183000@naver.com>
2022-12-07 15:13:30 +0900

    play버튼 클릭시 비디오가 있을경우 영상출력 기능

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,37 +1,38 @@
-import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
+import React, { useEffect, useState } from "react";
 import requests from "../api/requests";
 import "./Banner.css";
 import styled from "styled-components";
 export default function Banner() {
   const [movie, setMovie] = useState([]);
   const [isClicked, setIsClicked] = useState(false);
+
   useEffect(() => {
     fetchData();
   }, []);
-  const fetchData = async () => {
-    const request = await axios.get(requests.fetchNowPlaying);
-    // 현재 상영중인 영화  정보를 가져오기 (여러영화)
 
+  const fetchData = async () => {
+    // 현재 상영중인 영화 정보를 가져오기(여러 영화)
+    const request = await axios.get(requests.fetchNowPlaying);
+
+    // 여러 영화 중 영화 하나의 ID를 가져오기
     const movieId =
       request.data.results[
         Math.floor(Math.random() * request.data.results.length)
       ].id;
-    //여러 영화 중 영화 하나의 ID 가져오기
+
+    // 특정 영화의 더 상세한 정보를 가져오기(비디오 정보도 포함)
     const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
-      params: { append_to_reponse: "videos" },
+      params: { append_to_response: "videos" },
     });
     setMovie(movieDetail);
-    //data를 movieDetail로 설정후 setMovie에 movieDetail을 넣기
-    //특정 영화의 더 상세정보를 가져오기(비디오 정보 포함)
-    //eslint-disable-next-line no-unused-vars
-    // const truncate = (str, n) => {
-    //   return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-    // };
-    // 배너 스크립트의 글자수가 100자가 넘으면 99자 이후 ...이 나오도록 설정
-
-    // 정의하고 return에서 함수 실행을 하는데 오류떠서 잠시 제거 1. eslint가 읽지못하여 옆에 주석처리후 작성 시도 ->실패
   };
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
+  console.log("movie", movie);
   if (!isClicked) {
     return (
       <header
@@ -44,31 +45,61 @@ export default function Banner() {
       >
         <div className="banner__contents">
           <h1 className="banner__title">
-            {" "}
             {movie.title || movie.name || movie.original_name}
           </h1>
+
           <div className="banner__buttons">
             <button
               className="banner__button play"
               onClick={() => setIsClicked(true)}
             >
-              Play{" "}
+              Play
             </button>
             <button className="banner__button info">More Information</button>
           </div>
-          <h1 className="banner__description">{movie.overview}</h1>
+
+          <h1 className="banner__description">
+            {truncate(movie.overview, 100)}
+          </h1>
         </div>
-        <div className="banner__fadeBottom" />
+        <div className="banner--fadeBottom" />
       </header>
     );
   } else {
     return (
       <Container>
-        <HomeContainer>clicked</HomeContainer>
+        <HomeContainer>
+          <Iframe
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="autoplay; fullscreen "
+            allowfullscreen
+          ></Iframe>
+        </HomeContainer>
       </Container>
     );
   }
 }
+// 항상 대문자로 시작 변수정의
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const Container = styled.div`
   display: flex;
